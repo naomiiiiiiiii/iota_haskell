d8a58e7c3996d288e09d8c33944e9ad250765fa3
Naomi Schmad <naomispargo@Eddie.local>
2026-04-26 15:35:59 -0400

    scanning for symbols

--- src/Lexer/Lexer.hs
+++ src/Lexer/Lexer.hs
@@ -4,7 +4,7 @@
 module Lexer.Lexer (iotaScan)
 where
 
-import Data.Char (isAlpha, isAlphaNum)
+import Data.Char (isAlpha, isAlphaNum, isPunctuation, isSymbol)
 import Text.Read (readMaybe)
 
 ------------
@@ -44,3 +44,21 @@ intTok :: String -> Token
 intTok str = (maybe
               (error $ "Lexer.intTok: Expected numeric string, got " ++ str)
               Int) . readMaybe $ str
+
+-- @scanSymbol keywords front rem@ returns @(tok, rem') where @tok@ contains the shortest prefix of @front ++ rem@ that is also a (non-empty) symbolic token. @rem'@ is the remainder of @rem@ left unscanned.
+-- In other words, @scanSymbol@ scans over @rem@ from left to right. If @front@ becomes a symbolic keyword OR it sees a non-symbolic character in @rem@, it stops scanning and returns @front@ as a symbolic token. Otherwise, it appends the new symbolic character to @front@ and recurses on the remainder of @rem@.
+-- @scanSymbol@ assumes that @front@ is nonempty and comprised of symbols
+scanSymbol :: Keywords -> String -> String -> (Token, String)
+scanSymbol keyWs front back = do
+  case back of
+    [] -> (Key(front), back) -- @rem@ is empty, return all the symbols in @front@
+    hd:remBack -> if ((elem front (symbols keyWs)) || not (isSymbolic hd))
+                 then (Key(front), back)
+                 else scanSymbol keyWs (front ++ [hd]) remBack
+-- Line 47 is slow because of the singleton append. IDEA: build up front backwards, then reverse once at the end? Then I would have to reverse all members of @Keywords.symbols@ too for the comparison, so is it really faster?
+
+------------
+--- Lexer helpers
+------------
+isSymbolic :: Char -> Bool
+isSymbolic s = isSymbol s || isPunctuation s
