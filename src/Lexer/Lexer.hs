e8ebc01824518968bce1acaca56e3fb7c16a4b7c
Naomi Schmad <naomispargo@Eddie.local>
2026-04-25 20:58:42 -0400

    general @Keywords@ type for all lexers and value @iotaKeywords : Keywords@ for the iota lexer

--- src/Lexer/Lexer.hs
+++ src/Lexer/Lexer.hs
@@ -1,7 +1,22 @@
+{-# LANGUAGE NamedFieldPuns#-}
 
 -- A generic lexer implemented as described in *ML for the Working Programmer*/
 module Lexer.Lexer (iotaScan)
 where
 
-iotaScan :: ()
-iotaScan = error "TODO"
+
+------------
+--- Keywords
+------------
+data Keywords = Keywords {alphaNumeric :: [String] -- alpha-numeric keywords
+                        , symbols :: [String] -- keywords with symbols (non-alpha-numeric) characters
+                        , commentL :: Char
+                        , commentR :: Char}
+
+iotaKeywords :: Keywords
+iotaKeywords = Keywords {alphaNumeric, symbols, commentL, commentR}
+  where alphaNumeric = ["ret", "bind", "let", "ref", "in", "Nat", "Unit", "Ref", "Comp", "let"]
+        symbols = ["(", ")", "\\", ".", "=", ":=", "!", "->", "+", "*", "-", "/"]
+        commentL = '{' -- TODO, would be nice to have a multi-character comment intro like /* comment */
+        commentR = '}'
+
