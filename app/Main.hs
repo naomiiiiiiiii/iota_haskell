f4fdc2dbb254959a2f1210a3f0d07dacaf305bda
Naomi Schmad <naomispargo@Eddie.local>
2026-04-26 15:54:43 -0400

    REPL-like @main@ that just prints the output of the lexer for now

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main (main) where
 
-import Lib
+import System.IO (hFlush, stdout)
+import qualified Lexer.Lexer as Lexer
 
 main :: IO ()
-main = someFunc
+main = do
+  putStr ">> "
+  hFlush stdout
+  inStr <- getLine
+  let tokList = Lexer.iotaScan inStr
+  putStrLn (show tokList)
+  main
