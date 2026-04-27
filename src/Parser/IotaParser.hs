6856bf871f6e012792090d2dc98a051b0239cb0a
Naomi Spargo <nspargo@alumni.cmu.edu>
2026-04-27 19:19:06 -0400

    parse AST types and type-annotated variables

--- src/Parser/IotaParser.hs
+++ src/Parser/IotaParser.hs
@@ -10,13 +10,27 @@ import qualified Lexer.Lexer as Lex
 ------------
 -- The first combinators are the smallest and parse the simplest parts of the AST. These simple combinators are combined later in the file to parse larger parts of the AST. Eventually, we can define @letBinding@, which parses a toplevel let-binding of the form @let [str] = [AST.Exp]@
 
--- parse constant value
+-- parse constant values (Integers and Units)
 constant :: [Lex.Token] -> Either SyntaxError (AST.Exp, [Lex.Token])
 constant = (intP >>> AST.Int) |:| (unitP >>> (\_ -> AST.Star))
 
--- parse base types
+-- parse base types (Integer and Unit)
 baseTyp :: [Lex.Token] -> Either SyntaxError (AST.Typ, [Lex.Token])
 baseTyp = ((key "Int") >>> (\_ -> AST.IntTyp)) |:| ((key "Unit") >>> (\_ -> AST.UnitTyp))
+
+-- parse any AST type
+typ :: [Lex.Token] -> Either SyntaxError (AST.Typ, [Lex.Token])
+typ = ((circ (keyCircR atomTyp "->") atomTyp) >>> AST.ArrowTyp) -- case for function types
+      |:| ((keyCircR atomTyp "Ref") >>> AST.RefTyp) -- case for reference types
+      |:| ((keyCircR atomTyp "Comp") >>> AST.CompTyp) -- case for computations
+      |:| atomTyp -- case for base types or types wrapped in parentheses
+  where atomTyp = (baseTyp
+                   |:| (keyCircL ")" (keyCircR typ "(")))
+
+-- parse a type-annotated variable
+typedId :: [Lex.Token] -> Either SyntaxError ((String, AST.Typ), [Lex.Token])
+typedId = keyCircL ")" (circ typ -- parse the type
+                         (keyCircL ":" (keyCircR ident "("))) -- parse the identifier
 
 iotaParser :: ([Lex.Token] -> Either SyntaxError (a, [Lex.Token]))
 iotaParser = error "TODO"
