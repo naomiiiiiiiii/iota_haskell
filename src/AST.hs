c76547eb0985c3459dc7acd2b02b53428ca74c1e
Naomi Spargo <nspargo@alumni.cmu.edu>
2026-04-27 21:20:20 -0400

    deriving show and eq for types in AST.hs and some documentation

--- src/AST.hs
+++ src/AST.hs
@@ -8,9 +8,10 @@ data Typ = IntTyp
          | ArrowTyp (Typ, Typ) -- function
          | RefTyp Typ -- reference, written "Ref([Typ])"
          | CompTyp Typ -- suspended stateful computation, written "Comp([Typ])"
+  deriving(Show, Eq)
 
 data Exp = Free String
-         | Bound Int
+         | Bound Int -- This AST uses De Bruijn indices
          | Star -- @Star@ is the only value of type @Unit@, written "()"
          | Int Int
          | Loc Int -- location into the store. This is an internal expression, not accessible to the user.
@@ -22,3 +23,4 @@ data Exp = Free String
          | Ref Exp -- a reference to an expression, written "ref [Exp]"
          | Asgn (Exp, Exp) -- assign to a reference, written "[Exp] := [Exp]"
          | Deref Exp -- dereference a reference, written "![Exp]"
+  deriving(Show, Eq)
