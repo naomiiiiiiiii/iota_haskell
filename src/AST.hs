87ee5dc507a4247ca029e51c77cb5e0714b7ac0a
Naomi Spargo <nspargo@alumni.cmu.edu>
2026-04-27 21:21:23 -0400

    functions for constructing ASTs and traversing over ASTs

--- src/AST.hs
+++ src/AST.hs
@@ -1,5 +1,5 @@
 -- The AST for the Iota language and helper functions
-module AST (Typ(..) , Exp(..))
+module AST (Typ(..) , Exp(..), absList)
 where
 
 -- TODO: Add strings?
@@ -24,3 +24,49 @@ data Exp = Free String
          | Asgn (Exp, Exp) -- assign to a reference, written "[Exp] := [Exp]"
          | Deref Exp -- dereference a reference, written "![Exp]"
   deriving(Show, Eq)
+
+------------
+--- Functions for traversing over the AST
+------------
+
+-- At a high level, @mapExpWDepth@ is like @mapi :: (Int -> a -> b) -> [a] -> [b]@, but for expressions
+-- @mapi@ maps a function over each element of the argument list to get a new list. That function takes an element of the list, of course, but it also takes that element's index.
+-- In a similar way, @mapExpWDepth@ applies a function to each variable in the argument expression. That function has two components -- one component takes a string argument to handle free variables, the other component takes an integer argument to handle bound variables. Both function components take a second argument: the variable's depth. In the context of an expression, a variable's depth is how many binders deep that variable is. For instance, the variable @x@ in expression @x + 5@ is 0 binders deep. But the expression @x@ in @\(y x).x@ is two binders deep.
+-- Variable depth in @mapExpWDepth@ is analagous to element index in @mapi@.
+-- So @mapExpWDepth varCase startingIndex exp@ applies the appropriate function in @varCase@ to all the variables in @exp@. -- START HERE refactor to remove @start@, unnecessarily complicated.
+mapExpWDepth :: (Int -> (String -> Exp, Int -> Exp)) -> Int -> Exp -> Exp
+mapExpWDepth varCase start expr =
+    let mapExpWDepthHelp = mapExpWDepth varCase
+        varCases = varCase start in
+    case expr of
+    Free str -> (fst varCases) str
+    Bound i -> (snd varCases) i
+    Star -> expr
+    Int _ -> expr
+    Loc _ -> expr
+    Plus (exp1, exp2) -> Plus (mapExpWDepthHelp start exp1, mapExpWDepthHelp start exp2)
+    Lam (arg, body) -> Lam(arg, (mapExpWDepthHelp (start + 1)) body)
+    Ap(exp1, exp2) -> Ap((mapExpWDepthHelp start exp1), (mapExpWDepthHelp start exp2))
+    Ret(exp0) -> Ret(mapExpWDepthHelp start exp0)
+    Bind(exp1, (s, exp2)) -> Bind((mapExpWDepthHelp start exp1), (s, (mapExpWDepthHelp (start + 1) exp2)))
+    Ref(exp0) -> Ref(mapExpWDepthHelp start exp0)
+    Asgn(ref, exp0) -> Asgn((mapExpWDepthHelp start ref), (mapExpWDepthHelp start exp0))
+    Deref ref -> Deref(mapExpWDepthHelp start ref)
+
+------------
+--- Functions for constructing ASTs
+------------
+-- For variable index i >= 0, @abstract i var exp@ will turn all free occurences of variable @var@ in @exp@ into occurences of the bound variable indexed by @i@
+-- This function is useful when constructing a lambda abstraction. The lambda body starts out with only free variables. But, as each binder is added over the body expression, the corresponding free variable in the body is captured by the binder and converted to a bound variable
+abstract :: Int -> String -> Exp -> Exp
+abstract i boundVarStr = let varCase = \j -> ( let freeVarHandler = \varStr ->
+                                                               if (varStr == boundVarStr)
+                                                               then (Bound j)
+                                                               else (Free varStr) -- Free variables not named @boundVarStr@ are left alone
+                                                in (freeVarHandler, Bound))
+                         in mapExpWDepth varCase i
+
+-- @absList args body@ creates a lambda abstraction with arguments @args@ (by name and type) and body @body@
+-- For each argument in @args@, @absList@ binds all free occurences of that argument in @body@ to the right bound variable, based on the order of @args@.
+absList :: ([(String, Typ)], Exp) -> Exp
+absList (varList, body) = foldr (\typedVar bodyAcc -> Lam(typedVar, (abstract 0 (fst typedVar) bodyAcc))) body varList
