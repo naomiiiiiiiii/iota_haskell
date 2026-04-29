dc52f280699d2d67a57fa2ca56b66c52c8f91fd2
Naomi Spargo <nspargo@alumni.cmu.edu>
2026-04-29 10:27:55 -0400

    updated main to use interpreter

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,11 @@
 module Main (main) where
 
-import System.IO (hFlush, stdout)
-import qualified Lexer.Lexer as Lex
+import qualified Interpreter.Interpreter as Interp
+import qualified Interpreter.Environment as Env
 
+import qualified Data.Map as M
+import Control.Monad.State
+
+-- run the interpreter, starting with an empty store and empty global and local environments
 main :: IO ()
-main = do
-  putStr ">> "
-  hFlush stdout
-  inStr <- getLine
-  let tokList = Lex.scan Lex.iotaKeywords inStr
-  putStrLn (show tokList)
-  main
+main = fst <$> runStateT Interp.interpret (M.empty, Env.emptyEnv)
