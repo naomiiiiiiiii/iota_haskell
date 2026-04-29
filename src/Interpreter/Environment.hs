e0f3f2def970119381108f5b1fa69abe1ae67ff0
Naomi Spargo <nspargo@alumni.cmu.edu>
2026-04-29 13:14:14 -0400

    comment

--- src/Interpreter/Environment.hs
+++ src/Interpreter/Environment.hs
@@ -8,7 +8,9 @@ module Interpreter.Environment (Env(..), newEnv, addGlobalEnv, emptyEnv)
 import qualified AST as AST
 import qualified Data.Map as M
 
-
+------------
+--- Global and local environments
+------------
 -- When interpretting an expression, we have recourse to two environments
 -- The first is all previous global expression declarations, represented as a map from strings (variable names) to typed expressions.
 -- The second is all local bindings (introduced by lambdas), represented as a simple list of types. Lists work nicely with local bindings' De Bruijn indices: when I cons a new bound variable onto the local environment, it automatically has index 0, and all the other local bindings are automatically shifted up one.
@@ -24,4 +26,3 @@ addGlobalEnv key v env = env {globalEnv = M.insert key v (globalEnv env)}
 
 emptyEnv :: Env
 emptyEnv = Env M.empty []
-
