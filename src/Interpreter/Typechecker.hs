6c108f4f71286244b35634692b3a660797072611
Naomi Spargo <nspargo@alumni.cmu.edu>
2026-05-03 16:31:58 -0400

    typechecking base cases

--- src/Interpreter/Typechecker.hs
+++ src/Interpreter/Typechecker.hs
@@ -23,4 +23,10 @@ tcError msg = error ("Typechecker.typechecker" ++ msg)
 -- While doing this, it reads from the environment of global typed expression declarations. This environment is contained in the reader.
 -- @typeChecker@ also reads from and extends the environment of locally bound variable types (contained in the reader as well). Because this environment is only ever extended locally (when going under a binder), I think it is appropriate to use a @Reader@, rather than @State@, for this environment as well
 typeChecker :: AST.Exp -> Reader Env.Env AST.Typ
-typeChecker expr = error "TODO"
+typeChecker expr = case expr of
+    Free var -> fst <$> Env.lookUpGlobalU var
+    Bound i -> Env.lookUpLocalU i
+    Star -> return UnitTyp
+    Int _ -> return IntTyp
+    Loc i -> tcError ("uninitialized location: " ++ (show i))
+    --raw locations should only show up during execution, not when a program is typechecked
