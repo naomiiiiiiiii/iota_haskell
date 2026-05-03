955da89c20bfba4217345199cc39a96aa315b85f
Naomi Spargo <nspargo@alumni.cmu.edu>
2026-05-03 17:02:44 -0400

    simple cases (value cases and free variables) for reduction

--- src/Interpreter/Reducer.hs
+++ src/Interpreter/Reducer.hs
@@ -31,4 +31,13 @@ rError msg = error ("RUNTIME ERROR (Reducer.reduce) " ++ msg)
 -- While doing this, it reads from the environment of global typed expression declarations. This environment is contained in the reader.
 -- It also modifies and reads from the global memory store. This is contained in the state.
 reduce :: AST.Exp -> StateT (M.IntMap AST.Exp) (Reader Env.Env) AST.Exp
-reduce expr = error "TODO" 
+reduce expr = case expr of
+  Free ident -> (snd <$> (lift $ Env.lookUpGlobalU ident)) >>= reduce
+  Star -> return expr -- Value, cannot be reduced
+  Int _  -> return expr -- Value
+  Loc _ -> return expr -- Value
+  Lam _ -> return expr -- Value
+  Ret _ -> return expr -- Suspended computation
+  Ref _ -> return expr -- Suspended computation
+  Asgn _ -> return expr -- Suspended computation
+  Deref _ -> return expr  -- Suspended computation
