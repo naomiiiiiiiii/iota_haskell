7b765489e2eba9264d33bc1ebdc619fd007d3f02
Naomi Spargo <nspargo@alumni.cmu.edu>
2026-05-03 16:58:10 -0400

    error handling for expression reduction

--- src/Interpreter/Reducer.hs
+++ src/Interpreter/Reducer.hs
@@ -1,12 +1,31 @@
+{-# LANGUAGE ScopedTypeVariables#-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 -- Reduce Iota expressions to values
 
 module Interpreter.Reducer (reduce)
   where
 
-import qualified AST as AST
+import AST as AST
+import qualified Interpreter.Environment as Env
 import Control.Monad.Reader
-import Control.Monad.State
-import qualified Data.Map as M
+import Control.Monad.State.Strict -- ensures that Iota has strict semantics
+import qualified Data.IntMap as M
+import Prettyprinter
+
+------------
+--- Error handling
+------------
+
+-- throw an error, making it clear that it comes from the Reducer file
+-- Because of typechecking, this should never be called!
+rError :: String -> a
+rError msg = error ("RUNTIME ERROR (Reducer.reduce) " ++ msg)
+
+------------
+--- Evaluate expressions
+------------
 
 -- @reduce@ takes an expression and reduces it to a value
 -- While doing this, it reads from the environment of global typed expression declarations. This environment is contained in the reader.
