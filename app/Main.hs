c66aec94990442eaac12575b5ae1b1b99651dd7b
Naomi Spargo <nspargo@alumni.cmu.edu>
2026-05-03 21:09:11 -0400

    updated main to use new store type

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,8 @@ module Main (main) where
 import qualified Interpreter.Interpreter as Interp
 import qualified Interpreter.Environment as Env
 
-import qualified Data.IntMap as M
 import Control.Monad.State.Strict
 
 -- run the interpreter, starting with an empty store and empty global and local environments
 main :: IO ()
-main = fst <$> runStateT Interp.interpret (M.empty, Env.emptyEnv)
+main = fst <$> runStateT Interp.interpret (Env.emptyStore, Env.emptyEnv)
