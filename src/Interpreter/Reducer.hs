fcef4899ac3b2f07c8b87117ee75a40f0d055bc9
Naomi Spargo <nspargo@alumni.cmu.edu>
2026-05-03 21:08:15 -0400

    updated expression reduction to use the new store type

--- src/Interpreter/Reducer.hs
+++ src/Interpreter/Reducer.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE ScopedTypeVariables#-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 -- Reduce Iota expressions to values
@@ -12,7 +11,6 @@ import qualified Interpreter.Environment as Env
 import Control.Monad.Reader
 import Control.Monad.State.Strict -- ensures that Iota has strict semantics
 import qualified Data.IntMap as M
-import Prettyprinter
 
 ------------
 --- Error handling
@@ -30,7 +28,7 @@ rError msg = error ("RUNTIME ERROR (Reducer.reduce) " ++ msg)
 -- @reduce@ takes an expression and reduces it to a value
 -- While doing this, it reads from the environment of global typed expression declarations. This environment is contained in the reader.
 -- It also modifies and reads from the global memory store. This is contained in the state.
-reduce :: AST.Exp -> StateT (M.IntMap AST.Exp) (Reader Env.Env) AST.Exp
+reduce :: AST.Exp -> StateT Env.Store (Reader Env.Env) AST.Exp
 reduce expr = case expr of
   Free ident -> (snd <$> (lift $ Env.lookUpGlobalU ident)) >>= reduce
   Star -> return expr -- Value, cannot be reduced
@@ -60,30 +58,25 @@ reduce expr = case expr of
   Bound _ -> rError ("Cannot reduce bound variable: " ++ (show expr))
   -- @unsuspend comp@ expects @comp@ to be a fully-reduced computation (ie, an expression with @ret@, @ref@, @:=@, or @!@ at the top). It releases and runs the suspended computation @comp@.
   where
-    unsuspend :: AST.Exp -> StateT (M.IntMap AST.Exp) (Reader Env.Env) AST.Exp
+    unsuspend :: AST.Exp -> StateT Env.Store (Reader Env.Env) AST.Exp
     unsuspend comp = case comp of
      Ret exp0 -> reduce exp0
      Ref exp0 -> do
        val0 <- reduce exp0
        -- Add @val0@ to the store
-       nextIndex <- getNextKey
-       modify (\store -> M.insert nextIndex val0 store)
-       return $ Loc nextIndex
+       usedIndex <- Env.addToStore val0
+       return $ Loc usedIndex
      Asgn(loc, rhs) -> do
        index <- getIndex "cannot assign to non-location: " <$> reduce loc
        rhsVal <- reduce rhs
-       let errStr = "assigning to uninitialized location: " ++ (show loc)
        -- Update location @index@ to store @rhsVal@
-       modify (\store -> M.alter (\case
-                                     Just _ -> Just rhsVal
-                                     Nothing -> rError errStr)
-                         index
-                         store)
+       Env.modifyStore index rhsVal
        return Star -- Assignments don't return a meaningful value, they only update the store
+     -- TODO: move this reasoning out into @Env.lookupStore@
      Deref loc -> do
        index <- getIndex "cannot dereference non-location: " <$> reduce loc
        store <- get
-       case M.lookup index store of
+       case M.lookup index (Env.storeMap store) of
          Just out -> return out
          Nothing -> rError ("dereferencing uninitialized location: " ++ (show loc))
      _ -> rError ("expected computation, got " ++ (show comp))
@@ -94,15 +87,6 @@ reduce expr = case expr of
                             Loc index -> index
                             _ -> rError (errStr ++ (show loc))
 
--- @getNextKey@ returns the next available index into the store
--- TODO: This is a little slow, would be faster if you stored next key in the state
-    getNextKey :: (Monad b) => StateT (M.IntMap a) b Int
-    getNextKey = do
-      store <- get
-      return $ case M.lookupMax store of
-                 Just (maxKey, _) -> maxKey + 1
-                 Nothing -> 0 -- store is currently empty
-
 -- Given an AST binop, return the @Exp -> Exp -> Exp@ function that does what the @Bop@ means
 semanticOp :: Bop -> Exp -> Exp -> Exp
 semanticOp Plus e1 e2  =
@@ -112,11 +96,3 @@ semanticOp Plus e1 e2  =
       (Int _, _) -> rError (errStr ++ (show e2))
       _ -> rError (errStr ++ (show e1))
 
-------------
---- Pretty printing for the store
-------------
-instance (Pretty a) => Pretty (M.IntMap a) where
-  pretty store = braces $ concatWith (\x y -> x <> "," <+> y)
-                 (map (\(k, v) -> parens $ ("Loc:" <+> (pretty k) <> "," <+>
-                                          "Contents:" <+> (pretty v)))
-                   (M.assocs store))
