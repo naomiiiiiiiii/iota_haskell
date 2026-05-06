d609ca2d669ed2dd55aaf8f7bf6dd5ce424c2511
Naomi Spargo <nspargo@alumni.cmu.edu>
2026-05-06 12:51:35 -0400

    factored out the interpretation of a parsed let-binding into separate function @interpretBinding@

--- src/Interpreter/Interpreter.hs
+++ src/Interpreter/Interpreter.hs
@@ -12,7 +12,23 @@ import qualified AST as AST
 import System.IO (hFlush, stdout)
 import Control.Monad.State.Strict
 import Control.Monad.Reader
+import Data.Functor.Identity (runIdentity)
 import Prettyprinter
+
+-- Interpret a let-binding (@let [NAME] = [EXP]@) and update the state and global environment
+-- Returns the value and type of the bound variable
+interpretBinding :: (String, AST.Exp) -> State (Env.Store, Env.Env) (AST.Exp, AST.Typ)
+interpretBinding (name, expr) = do
+  (initStore, initEnv) <- get
+  let expType = runReader (TC.typeChecker expr) initEnv
+  let (reducedExp, finalStore) = runReader
+                                 (runStateT (R.reduce expr) initStore)
+                                 initEnv
+  -- update the state to the new store
+  modify (\(_, env) -> (finalStore, env))
+  -- update the state to the new environment
+  modify (\(store, oldEnv) -> (store, Env.addGlobalEnv name (expType, reducedExp) oldEnv))
+  return (reducedExp, expType)
 
 -- Run a REPL that interprets a sequence of Iota let-bindings (provided through @stdin@). After each let-binding, the REPL prints the resulting evaluated value and memory store.
 -- The ambient state consists first of the global, mutable memory store (a map of locations to expressions) and second of the variable environment
