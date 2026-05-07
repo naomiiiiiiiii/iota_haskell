419a80eb36307189a7c1e68c558793037beb8bb4
Naomi Spargo <nspargo@alumni.cmu.edu>
2026-05-06 21:50:26 -0400

    tests for arithmetic and a function reference

--- test/LargeTests.hs
+++ test/LargeTests.hs
@@ -33,7 +33,15 @@ examples = M.fromList
   [("Int reference (value 5)", [ "let test = bind(ret(5), \\n.bind(ref(0), \\r.bind(r:=n, \\u.bind(!r, \\m.ret m))))"])
  , ("Int reference (value -42)", ["let rneg42 = bind(ref(-1), \\r.bind(r := -42, \\u.bind(!r, \\m.ret m)))"])
  , ("Lambdas", ["let second = \\(x : Int).\\(y : Int).\\(z : Int).y + 100"
-              , "let out = second 1 2 3"])]
+              , "let out = second 1 2 3"])
+ , ("Arith", ["let plus = \\(n1 : Int).\\(n2 : Int).n1 + n2"
+            , "let four = plus (plus 1 1) 2"])
+ , ("Function reference", ["let plus = \\(n1 : Int).\\(n2 : Int).n1 + n2"
+                         , "let plusref = ref plus"
+                         , "let plus1 = \\(n2 : Int).bind(plusref, \\fref.bind(!fref, \\f.ret(f 1 n2)))"
+                         , "let suspendedTwo = plus1 1"
+                         , "let two = bind(suspendedTwo, \\n.ret(n))"])
+ ]
 
 -- @parsingResults@ contains the expected results of parsing the large examples we test on, organized by name
 -- The example consists of a list of let-bindings. Each let-binding in the example is parsed to get the LHS of the binding (a string name) and the RHS of the binding (an expression).
@@ -43,6 +51,12 @@ parsingResults = M.fromList [
  ,("Int reference (value -42)", [("rneg42",Bind (Ref (Int (-1)),("r",Bind (Asgn (Bound 0,Int (-42)),("u",Bind (Deref (Bound 1),("m",Ret (Bound 0))))))))])
  ,("Lambdas", [("second",Lam (("x",IntTyp),Lam (("y",IntTyp),Lam (("z",IntTyp),Binop (Plus,Bound 1,Int 100)))))
               ,("out",Ap (Ap (Ap (Free "second",Int 1),Int 2),Int 3))])
+ ,("Arith", [("plus",Lam (("n1",IntTyp),Lam (("n2",IntTyp),Binop (Plus,Bound 1,Bound 0)))),("four",Ap (Ap (Free "plus",Ap (Ap (Free "plus",Int 1),Int 1)),Int 2))])
+ ,("Function reference", [("plus",Lam (("n1",IntTyp),Lam (("n2",IntTyp),Binop (Plus,Bound 1,Bound 0))))
+                         ,("plusref",Ref (Free "plus"))
+                         ,("plus1",Lam (("n2",IntTyp),Bind (Free "plusref",("fref",Bind (Deref (Bound 0),("f",Ret (Ap (Ap (Bound 0,Int 1),Bound 2))))))))
+                         ,("suspendedTwo",Ap (Free "plus1",Int 1))
+                         ,("two",Bind (Free "suspendedTwo",("n",Ret (Bound 0))))])
   ]
 
 -- @typingResults@ contains the expected results of typing the large examples we test on, organized by name. There is one type for each let-binding in the large example.
@@ -51,7 +65,15 @@ typingResults = M.fromList [
   ("Int reference (value 5)", [CompTyp IntTyp])
  ,("Int reference (value -42)", [CompTyp IntTyp])
  ,("Lambdas", [ArrowTyp (IntTyp,ArrowTyp (IntTyp,ArrowTyp (IntTyp,IntTyp)))
-              ,IntTyp])]
+              ,IntTyp])
+ ,("Arith", [ArrowTyp (IntTyp,ArrowTyp (IntTyp,IntTyp))
+            ,IntTyp])
+ ,("Function reference", [ArrowTyp (IntTyp,ArrowTyp (IntTyp,IntTyp))
+                         ,CompTyp (RefTyp (ArrowTyp (IntTyp,ArrowTyp (IntTyp,IntTyp))))
+                         ,ArrowTyp (IntTyp,CompTyp IntTyp)
+                         ,CompTyp IntTyp
+                         ,CompTyp IntTyp])
+ ]
 
 -- @reduceResults@ contains the expected results of reducing the large examples we test on, organized by name. There is one value for each let-binding in the large example.
 reduceResults :: M.Map String [AST.Exp]
@@ -59,4 +81,13 @@ reduceResults = M.fromList [
   ("Int reference (value 5)", [Ret (Int 5)])
  ,("Int reference (value -42)", [Ret (Int $ -42)])
  ,("Lambdas", [Lam (("x",IntTyp),Lam (("y",IntTyp),Lam (("z",IntTyp),Binop (Plus,Bound 1,Int 100))))
-              ,Int 102])]
+              ,Int 102])
+ ,("Arith", [Lam (("n1",IntTyp) ,Lam (("n2",IntTyp),Binop (Plus,Bound 1,Bound 0)))
+            ,Int 4])
+ ,("Function reference", [Lam (("n1",IntTyp)
+                         ,Lam (("n2",IntTyp)
+                         ,Binop (Plus,Bound 1,Bound 0)))
+                         ,Ref (Free "plus")
+                         ,Lam (("n2",IntTyp),Bind (Free "plusref",("fref",Bind (Deref (Bound 0),("f",Ret (Ap (Ap (Bound 0,Int 1),Bound 2)))))))
+                         ,Ret (Ap (Ap (Lam (("n1",IntTyp),Lam (("n2",IntTyp),Binop (Plus,Bound 1,Bound 0))),Int 1),Int 1))
+                         ,Ret (Int 2)])]
