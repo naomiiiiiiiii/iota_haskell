a919e177297e3b5c128e53b5e2d1fd394e8d24b7
Naomi Spargo <nspargo@alumni.cmu.edu>
2026-05-06 22:40:31 -0400

    added unit tests to toplevel testing function

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,6 @@
 import Test.Tasty (defaultMain, testGroup, TestTree)
 import qualified LargeTests as LargeTests
+import qualified ParserCombTests as PCTests
 
 main :: IO ()
 main = defaultMain tests
@@ -8,4 +9,4 @@ tests :: TestTree
 tests = testGroup "All tests" [LargeTests.tests, unitTests]
 
 unitTests :: TestTree
-unitTests = testGroup "Unit tests" [] -- TODO
+unitTests = testGroup "Unit tests" [PCTests.tests] -- TODO add unit tests for iota's parser, lexer, typechecking, reduction
